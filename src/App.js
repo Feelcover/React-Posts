21434888a980dac88c81097edf7601b4b59e2a49
Andrey Chebanov <pioneerbeat@yandex.ru>
2022-07-11 17:27:10 +0300

    рефакторинг апи, добавленик кастомного хука useFetching, реализация погинации

--- src/App.js
+++ src/App.js
@@ -11,36 +11,46 @@ import { usePosts } from "./components/hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./components/hooks/useFetching";
+import { getPageCount, getPagesArray } from "./utils/pages";
+import Pagination from './components/UI/Pagination/Pagination'
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-  const [isPostsLoading, setIsPostsLoading] = useState (false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
+
 
   useEffect(() => {
-    fetchPosts()
-  }, [])
+    fetchPosts();
+  }, [page]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts)
-    setIsPostsLoading(false);
-  }
-
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+
+
+  const changePage = (page) => {
+    setPage(page)
+  }
 
   return (
     <div className="App">
@@ -52,15 +62,20 @@ function App() {
       <div className="createposts">Создать пост</div>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {isPostsLoading
-          ? <Loader/>
-          : <PostList
+      {isPostsLoading ? (
+        <Loader />
+      ) : (
+        <PostList
           remove={removePost}
           posts={sortedAndSearchedPosts}
           title={"Список постов"}
         />
-      }
-      
+      )}
+      <Pagination 
+      page={page}
+      changePage={changePage}
+      totalPages={totalPages}
+      />
     </div>
   );
 }
