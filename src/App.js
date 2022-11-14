2c1690c30718c8119896c7294e9e0f941907db3f
Andrey Chebanov <pioneerbeat@yandex.ru>
2022-11-14 15:22:28 +0300

    add/deploy-ghpages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AppRouter from "./components/AppRouter";
 import Navbar from "./components/UI/Navbar/Navbar";
 import { AuthContext } from "./context";
 
-// import Posts from "./pages/Posts";
 import "./styles/App.css";
 
 function App() {
